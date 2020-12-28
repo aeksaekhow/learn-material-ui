405137ac1f52782bc38401c5628106eae89ecf9c
Aek Sae-khow <saekhow.aek@gmail.com>
2020-12-28 10:11:33 +0700

    - Add assets and animations directories

---
+++ project1/src/components/ui/Header.js
@@ -0,0 +1,15 @@
+import {
+AppBar,
+Toolbar
+} from '@material-ui/core'
+
+
+export default function Header(props) {
+
+    return (
+        <AppBar>
+            <Toolbar>Hello</Toolbar>
+        </AppBar>
+    )
+
+}
